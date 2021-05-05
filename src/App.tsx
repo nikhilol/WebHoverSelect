d2db0158799754a3b897c934e5f450ea521a38f1
nikhilol <nikhil.patel.np@outlook.com>
2021-05-05 18:44:17 +0100

    Styling and number extraction algorithm refactored

--- src/App.tsx
+++ src/App.tsx
@@ -4,55 +4,19 @@ import './App.css';
 
 function App() {
 
-  const [activeElement, setActiveElement] = useState<EventTarget | null>()
-
-  useEffect(()=>{
-    console.log(activeElement)
-  },[activeElement])
-
-  function setActive(e: Event) {
-    e.preventDefault()
-    setActiveElement(e.target)
-    return
-  }
+  const [activeElement, setActiveElement] = useState<Element | null>()
 
   useEffect(() => {
-    prepare()
-  })
+    console.log(activeElement)
+  }, [activeElement])
 
-  function prepare(){
-    let elements: NodeListOf<Element> = document.getElementsByTagName('body')[0].querySelectorAll('*')
-    console.log(elements)
-    let arrayOfElements = Array.from(elements)
-    let textElementArr: Element[] = []
-    arrayOfElements.forEach(element => {
-      if (element.textContent && element.nodeName !== 'NOSCRIPT') {
-        element.className += ' hvtool'
-        element.addEventListener('click', (e) => {return setActive(e)})
-        textElementArr.push(element)
-      }
-      // element.className +=   ' ' + arrayOfElements.indexOf(element);
-    })
-    console.log(textElementArr)
-    return
-  }
+  chrome.runtime.onInstalled.addListener(() => {
+    console.log('Working')
+  })
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
